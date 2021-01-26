6d3135048146b3b6347b71bf5c3ebcb1496cadfb
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-25 19:30:14 -0600

    Configure controllers to respond to json request.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
   
+  respond_to :json
   protect_from_forgery with: :exception
   before_action :underscore_params!
 
