331437bfb2c2f311b797ecac85d9c601cc66992f
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-25 21:49:55 -0600

    Create authenticate user method (include response and exception handler).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,27 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
+  include Response
+  include ExceptionHandler
   
   respond_to :json
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def authenticate_user
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+        
+        @current_user_id = jwt_payload['id']
+      end
+    end
   end
 end
