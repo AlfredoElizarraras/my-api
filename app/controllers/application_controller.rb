62e3af4122dd9211d66f885cac4195949646cad1
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-25 21:41:23 -0600

    Configure Devise to allow usernames on sign up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::API
   
   respond_to :json
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
